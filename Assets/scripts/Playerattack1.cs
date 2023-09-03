3136ad163da3ccabfc4954e5f532b912c7362b89
E9ex <egecansar@gmail.com>
2023-09-03 12:18:42 +0300

    fixedfilesystem

---
+++ Assets/scripts/Playerattack1.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerattack_Throw : MonoBehaviour
+{
+    [SerializeField] private LineRenderer lr;
+    [SerializeField] private Joystick attackjoystick;
+    [SerializeField] private Transform attacklookat;
+    [SerializeField] public float lrdistance = 7f;
+    [SerializeField] private Transform player;
+    private RaycastHit hit;
+    private bool shoot;
+    [SerializeField] private Transform bullet;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(attackjoystick.Horizontal)>.5f || Mathf.Abs(attackjoystick.Vertical)>.5f)
+        {
+            // if (lr.gameObject.activeInHierarchy==false)
+            // {
+            //     lr.gameObject.SetActive(true);
+            // }
+            transform.position = new(player.position.x, .1f, player.position.z);
+            attacklookat.position = new Vector3(attackjoystick.Horizontal+transform.position.x, .1f, attackjoystick.Vertical+transform.position.z);
+            transform.LookAt(new Vector3(attacklookat.position.x,0,attacklookat.position.z));
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            lr.SetPosition(0,transform.position);//başlangıç noktasını 0. pozisyonunu nesnenin mevcut konumuna ayarlar
+            if (Physics.Raycast(transform.position,transform.forward,out hit,lrdistance))
+            {
+                lr.SetPosition(1,hit.point);//1 pozisyonunu, bu çarpışma noktasına ayarlar. (bunu yazmazsam obstacleslara gitmiyor linerenderer.)
+            }
+            else
+            {
+              lr.SetPosition(1,transform.forward+transform.forward*lrdistance);//Bu, çarpışma olmadığında lazer hattının belirtilen uzaklıkta düz bir çizgi olarak görüneceği demek.(bunu yazmazsam ise sadece obstaclelari gidiyor linerenderer.)
+                lr.SetPosition(1,new Vector3(lr.GetPosition(1).x,.1f,lr.GetPosition(1).z));//çarpışma olmadığında, lazer hattının ikinci noktasını ayarlar.
+            }
+            if (shoot==false)
+            {
+                shoot=true;
+            }
+        }
+        else if (shoot&&Input.GetMouseButtonUp(0))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            shoot = false;
+        }
+        // else if (lr.gameObject.activeInHierarchy==true)
+        // {
+        //     lr.gameObject.SetActive(false);
+        // }
+    }
+}
