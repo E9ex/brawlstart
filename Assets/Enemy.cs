3ad04b49805e49a9ca2ad713a10c187b3f9db565
E9ex <egecansar@gmail.com>
2023-09-05 23:10:38 +0300

    animchanges

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -12,6 +12,9 @@ public class Enemy : MonoBehaviour
   //  private Transform enemy;
     private Animator forwardsandbackwards;
     public GameObject deathExp;
+    [SerializeField] private GameObject[] waypoints;//waypoints dizisi objelerin hareket edeceği noktaları tutacak.
+    private int currentWayPointIndex = 0; // waypoints dizisindeki o anki hedef noktanın index numarasını tutar.
+    [SerializeField] private float speed = 2f;//waypoints üzerinde hareket eden objelerin hızını tutacak.
 
     private void Awake()
     {
@@ -25,11 +28,23 @@ public class Enemy : MonoBehaviour
         healthbar.setmaxhealth(maxhealth);
     }
 
-    // private void Update()
-    // {
-    //     
-    //     animation();
-    // }
+ 
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(waypoints[currentWayPointIndex].transform.position.x, waypoints[currentWayPointIndex].transform.position.y, transform.position.z);
+
+        if (Vector2.Distance(new Vector2(targetPosition.x, targetPosition.y), new Vector2(transform.position.x, transform.position.y)) < 0.1f)
+        {
+            currentWayPointIndex++;
+
+            if (currentWayPointIndex >= waypoints.Length)
+            {
+                currentWayPointIndex = 0;
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+    }
 
     public void takedamage(int damage)
     {
