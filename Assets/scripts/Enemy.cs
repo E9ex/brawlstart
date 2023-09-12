1c6d4bd712bb96a7e652de346c2868428419a99a
E9ex <egecansar@gmail.com>
2023-09-13 01:37:18 +0300

    aıadded

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
@@ -18,10 +20,24 @@ public class Enemy : MonoBehaviour
     private bool isGoingForward = true;
 
 
+    [Header("navmesh")]
+    public NavMeshAgent agent;
+    public float range; //radius of sphere
+
+    public Transform centrePoint; 
+    
+    
+    
+    
+    
+    
+
+
     private void Awake()
     {
         forwardsandbackwards = GetComponent<Animator>();
-      //  enemy = GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        //  enemy = GetComponent<Transform>();
     }
 
     private void Start()
@@ -32,51 +48,67 @@ public class Enemy : MonoBehaviour
 
  
     void Update()
+    
     {
-        if (waypoints.Length == 0)
+        if(agent.remainingDistance <= agent.stoppingDistance) //done with path
         {
-            return;
-        }
-
-        Vector3 targetPosition = waypoints[currentWayPointIndex].position;
-
-        float distanceToWaypoint = Vector3.Distance(transform.position, targetPosition);
-
-        if (distanceToWaypoint < 0.1f)
-        {
-            if (isGoingForward)
+            Vector3 point;
+            if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
             {
-                currentWayPointIndex++;
-                if (currentWayPointIndex >= waypoints.Length)
-                {
-                    currentWayPointIndex = waypoints.Length - 2;
-                    isGoingForward = false;
-                }
-            }
-            else
-            {
-                currentWayPointIndex--;
-                if (currentWayPointIndex < 0)
-                {
-                    currentWayPointIndex = 1;
-                    isGoingForward = true;
-                }
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); //so you can see with gizmos
+                agent.SetDestination(point);
             }
         }
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
-        // Vector3 targetPosition = new Vector3(waypoints[currentWayPointIndex].transform.position.x, waypoints[currentWayPointIndex].transform.position.y, transform.position.z);
-        //
-        // if (Vector2.Distance(new Vector2(targetPosition.x, targetPosition.y), new Vector2(transform.position.x, transform.position.y)) < 0.1f)
+
+    }
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+
+        Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
+        { 
+            //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
+            //or add a for loop like in the documentation
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    
+        // if (waypoints.Length == 0)
         // {
-        //     currentWayPointIndex++;
-        //
-        //     if (currentWayPointIndex >= waypoints.Length)
-        //     {
-        //         currentWayPointIndex = 0;
-        //     }
+        //     return;
         // }
         //
+        // Vector3 targetPosition = waypoints[currentWayPointIndex].position;
+        //
+        // float distanceToWaypoint = Vector3.Distance(transform.position, targetPosition);
+        //
+        // if (distanceToWaypoint < 0.1f)
+        // {
+        //     if (isGoingForward)
+        //     {
+        //         currentWayPointIndex++;
+        //         if (currentWayPointIndex >= waypoints.Length)
+        //         {
+        //             currentWayPointIndex = waypoints.Length - 2;
+        //             isGoingForward = false;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         currentWayPointIndex--;
+        //         if (currentWayPointIndex < 0)
+        //         {
+        //             currentWayPointIndex = 1;
+        //             isGoingForward = true;
+        //         }
+        //     }
+        // }
         // transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+        
     }
 
     public void takedamage(int damage)
