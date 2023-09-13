1b3dcb0ececc9353d051663879c650e3085d118e
E9ex <egecansar@gmail.com>
2023-09-13 18:46:15 +0300

    updated

--- Assets/scripts/playermovement.cs
+++ Assets/scripts/playermovement.cs
@@ -8,13 +8,21 @@ public class playermovement : MonoBehaviour
     private bool movement;
 
     public float Velocity;
+    
     public float speed = 5;
     Vector3 lastPosition;
+    
+    [Header("anim")]
+    public float animforvelo=0.0f;
+    public float Acceleration = .1f;
+    private int velocityhash;
+    
     
     void Start()
     {
         playersprite.gameObject.SetActive(false);
         M_Camera.I.StartCamera(transform);
+        velocityhash = Animator.StringToHash("Velocityy");
     }
 
     void CalculateVelocity()
@@ -29,25 +37,19 @@ public class playermovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (joystick.Horizontal>0|| joystick.Horizontal<0 || joystick.Vertical>0|| joystick.Vertical<0)
+        if (joystick.Horizontal > 0 || joystick.Horizontal < 0 || joystick.Vertical > 0 || joystick.Vertical < 0)
         {
             playersprite.gameObject.SetActive(true);
-            playersprite.position = new Vector3(joystick.Horizontal+transform.position.x, .1f, joystick.Vertical+transform.position.z);
-            transform.LookAt(new Vector3(playersprite.position.x,0,playersprite.position.z));
+            playersprite.position = new Vector3(joystick.Horizontal + transform.position.x, .1f,
+                joystick.Vertical + transform.position.z);
+            transform.LookAt(new Vector3(playersprite.position.x, 0, playersprite.position.z));
             transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
             transform.Translate(Vector3.forward * Time.deltaTime * 5);
-            if (anim.GetBool("walking")!=true)
-            {
-                anim.SetBool("walking",true);
-            }
 
-            movement = true;
+            Velocity += Time.deltaTime * Acceleration;
+            anim.SetFloat(velocityhash, animforvelo);
         }
-        else if (movement==true)
-        {
-            anim.SetBool("walking",false);
-            movement = false;
-        }
+
         CalculateVelocity();
         
         
